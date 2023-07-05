a8b6d55ea87ce640900df3d48c9364575721e853
meowhard <work.aak@yandex.ru>
2023-07-05 03:18:10 +0300

    Вроде ровно
    наверное

--- src/main/java/ru/meowhard/preproject23/controller/AdminController.java
+++ src/main/java/ru/meowhard/preproject23/controller/AdminController.java
@@ -24,45 +24,9 @@ public class AdminController {
     }
 
     @GetMapping
-    public String showUserList(Model model, Principal principal) {
+    public String admin(Model model, Principal principal) {
         model.addAttribute("users", userDetailsService.getAllUsers());
         model.addAttribute("user", userDetailsService.getUserByName(principal.getName()));
         return "admin";
-    }
-
-    @GetMapping("/new")
-    public String getCreateUserForm(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        return "create_user";
-    }
-
-    @PostMapping
-    public String saveUser(@ModelAttribute("user") User user, Model model) {
-        user.setRoles(List.of(new Role(2L, "ROLE_USER")));
-        if(!userDetailsService.saveUser(user)) {
-            model.addAttribute("usernameErr", "Это имя пользователя уже занято");
-            return "create_user";
-        }
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String getEditUserForm(@PathVariable Long id, Model model) {
-        model.addAttribute("user", userDetailsService.getUserById(id));
-        return "edit_user";
-    }
-
-    @PostMapping("/edit/{id}")
-    public String updateUser(@PathVariable Long id, @ModelAttribute("user") User user, Model model) {
-        User existingUser = userDetailsService.getUserById(id);
-        userDetailsService.updateUser(user, existingUser);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/deleteUser/{id}")
-    public String deleteUser(@PathVariable Long id) {
-        userDetailsService.deleteUserById(id);
-        return "redirect:/admin";
     }
 }
